3aaec728dda311afbbf5b59f4a99e7782d4a3c81
Michel Shpilevsky <68427995+Nicheye@users.noreply.github.com>
2024-01-21 18:57:54 +0100

    3dfix

--- front/src/components/Threed.jsx
+++ front/src/components/Threed.jsx
@@ -10,7 +10,7 @@ const ThreeDModel = () => {
 
   const handleCreated = ({ gl, camera, scene }) => {
     // Add OrbitControls
-    
+    const controls = new OrbitControls(camera,gl.domElement);
     controls.enableDamping = true;
     controls.dampingFactor =0.25;
     controls.screenSpacePanning = false;
